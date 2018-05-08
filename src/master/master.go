40341536f840433937e5677202ff9e3e64ec7b77
jlin816 <lnj@mit.edu>
2018-05-08 02:51:48 -0400

    Add register requester/friend code

--- src/master/master.go
+++ src/master/master.go
@@ -24,6 +24,7 @@ const friendTimeout = 200 * time.Millisecond
 type Master struct {
     mu      sync.Mutex
 	friends []FriendData
+    requesters []RequesterData
 }
 
 type FriendData struct {
@@ -35,7 +36,46 @@ type FriendData struct {
 	available	bool // alive and not busy
 }
 
+type RequesterData struct {
+    id          int
+}
+
 // ====== RPC METHODS ===========
+
+func (mr *Master) RegisterFriend(args RegisterFriendArgs, reply *RegisterFriendReply) error {
+    mr.mu.Lock()
+    defer mr.mu.Unlock()
+
+	addr, err := net.ResolveTCPAddr("tcp", args.Address)
+	if err != nil {
+		return errors.New("Can't resolve friend's TCP addr")
+	}
+
+    newFriend := FriendData {
+		id: len(mr.friends),
+		address: addr,
+		available: true,
+		lastActive: time.Now(),
+	}
+    mr.friends = append(mr.friends, newFriend)
+    fmt.Printf("Connected friend %d!\n", newFriend.id)
+	reply.Success = true
+	return nil
+}
+
+func (mr *Master) RegisterRequester(args RegisterRequesterArgs, reply *RegisterRequesterReply) error {
+	mr.mu.Lock()
+	defer mr.mu.Unlock()
+
+    newRequester := RequesterData {
+        id: len(mr.requesters),
+    }
+    mr.requesters = append(mr.requesters, newRequester)
+    fmt.Printf("Connected new requester %d!\n", newRequester.id)
+    reply.Success = true
+	return nil
+}
+
 func (mr *Master) StartJob(args StartJobArgs, reply *StartJobReply) error {
 	fmt.Println("StartJob called")
 	mr.mu.Lock()
@@ -60,36 +100,10 @@ func (mr *Master) StartJob(args StartJobArgs, reply *StartJobReply) error {
 }
 
 // ====== PRIVATE METHODS =========
+
 func initMaster() (*Master) {
     mr := &Master{friends: []FriendData{}}
     return mr
-}
-
-func (mr *Master) RegisterFriend(args RegisterFriendArgs, reply *RegisterFriendReply) error {
-    mr.mu.Lock()
-    defer mr.mu.Unlock()
-
-	addr, err := net.ResolveTCPAddr("tcp", args.Address)
-	if err != nil {
-		return errors.New("Can't resolve friend's TCP addr")
-	}
-
-    newFriend := FriendData{
-		id: len(mr.friends),
-		address: addr,
-		available: true,
-		lastActive: time.Now(),
-	}
-    mr.friends = append(mr.friends, newFriend)
-    fmt.Printf("Connected friend %d!\n", newFriend.id)
-	reply.Success = true
-	return nil
-}
-
-func (mr *Master) RegisterRequester(args RegisterRequesterArgs, reply *RegisterRequesterReply) error {
-	mr.mu.Lock()
-	defer mr.mu.Unlock()
-	return nil
 }
 
 func main() {
